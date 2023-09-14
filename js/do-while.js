007e18c7259b923e39ab05e6532c704938f4b690
Juani <https://github.com/Juanilor>
2023-09-14 20:23:20 -0300

    acomodado del Do While

--- js/do-while.js
+++ js/do-while.js
@@ -1,8 +1,18 @@
-let edadCliente = parseInt(prompt('Ingrese una edad'))
+let edadCliente
+
+do(
+
+    edadCliente = parseInt(prompt('ingrese una edad valida'))
+)
+while(isNaN(edadCliente) || edadCliente > 120 || edadCliente <= 0)
+
+
+
+/*let edadCliente = parseInt(prompt('Ingrese una edad'))
 
 while(isNaN(edadCliente) || edadCliente > 120 || edadCliente <= 0){
     console.log(edadCliente)
 
     edadCliente = parseInt(prompt('ingrese una edad valida'))
 
-}
+}*/
